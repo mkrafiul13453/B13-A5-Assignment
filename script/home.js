af8a6788fe39521aaab2d8be517da74e13fa62d2
mkrafiul13453 <mkrafiul13453@gamil.com>
2026-03-08 23:25:52 +0600

    complete the modal and also add it in the card

--- script/home.js
+++ script/home.js
@@ -45,3 +45,38 @@ function displayIssues(issues) {
             displayIssues(filtered);
         }
     };
+
+
+    const showModal = (id) => {
+        const issue = allIssues.find(item => (item._id || item.id) === id);
+
+        if (issue) {
+            document.getElementById("modal-title").innerText = issue.title;
+            document.getElementById("modal-description").innerText = issue.description;
+            document.getElementById("modal-author").innerText = `• Opened by ${issue.author}`;
+            document.getElementById("modal-date").innerText = `• ${issue.createdAt}`;
+            document.getElementById("modal-priority").innerText = issue.priority;
+            document.getElementById("modal-assignee").innerText = issue.author;
+
+            
+            const modalStatus = document.getElementById("modal-status");
+            modalStatus.innerText = issue.status === 'open' ? 'Opened' : 'Closed';
+            modalStatus.className = issue.status === 'open' ?
+                "bg-green-500 text-white px-3 py-1 rounded-full text-xs font-semibold" :
+                "bg-blue-500 text-white px-3 py-1 rounded-full text-xs font-semibold";
+
+            
+            const modalTags = document.getElementById("modal-tags");
+            modalTags.innerHTML = issue.labels.map(label => `
+            <span class="text-red-500 border border-red-300 bg-red-50 px-3 py-1 rounded-full text-xs font-medium uppercase">
+                ${label}
+            </span>
+        `).join('');
+
+            
+            const modal = document.getElementById("issue_modal");
+            modal.showModal();
+        }
+    };
+
+    loadIssues();
