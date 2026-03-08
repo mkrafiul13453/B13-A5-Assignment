bdb3232a5fc8e55507d89d6e73393ac816473787
mkrafiul13453 <mkrafiul13453@gamil.com>
2026-03-08 22:10:57 +0600

    load all issues fromm the API

--- script/home.js
+++ script/home.js
@@ -1,2 +1,10 @@
 const cardContainer = document.getElementById("card-container");
 let allIssues = []; 
+
+
+async function loadIssues() {
+    const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
+    const data = await res.json();
+    allIssues = data.data;
+    displayIssues(allIssues);
+}
